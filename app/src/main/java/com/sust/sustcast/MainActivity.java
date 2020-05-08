968dc950912675a7f9d6f73cfe1757ebd625048b
istiakshihab <istiak@pm.me>
2020-05-08 17:21:27 +0600

    Splash Screen Added

--- app/src/main/java/com/sust/sustcast/MainActivity.java
+++ app/src/main/java/com/sust/sustcast/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, LandingActivity.class));
+        finish();
     }
 }
