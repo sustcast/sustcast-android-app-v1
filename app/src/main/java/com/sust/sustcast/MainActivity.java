480337a8aab1974e41ff63fcdbbd3f065d74c63a
istiakshihab <istiak@pm.me>
2020-05-08 21:13:01 +0600

    Added Logo Files

--- app/src/main/java/com/sust/sustcast/MainActivity.java
+++ app/src/main/java/com/sust/sustcast/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+        startActivity(new Intent(MainActivity.this, LandingActivity.class));
         finish();
     }
 }
