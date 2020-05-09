9713babc2494b5a9ad75829323cc4940437b01a5
appledora <embers1010@gmail.com>
2020-05-10 05:02:54 +0600

    unpolished Fragment layout

--- app/src/main/java/com/sust/sustcast/MainActivity.java
+++ app/src/main/java/com/sust/sustcast/MainActivity.java
@@ -1,14 +1,50 @@
 package com.sust.sustcast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(StreamFragment.newInstance("", ""));
+
     }
+
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_stream_frag:
+                            openFragment(StreamFragment.newInstance("", ""));
+                            return true;
+                        case R.id.nav_news_frag:
+                            openFragment(NewsReaderFragment.newInstance("", ""));
+                            return true;
+                        case R.id.nav_feedback_frag:
+                            openFragment(FeedbackFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
