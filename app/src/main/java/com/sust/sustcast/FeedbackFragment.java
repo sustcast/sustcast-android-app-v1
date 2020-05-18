d4f8c79bc606fe905a3907bca3ec126bd29fb70b
appledora <embers1010@gmail.com>
2020-05-19 04:39:06 +0600

    FeedBack Fragment structure @appledora

--- app/src/main/java/com/sust/sustcast/FeedbackFragment.java
+++ app/src/main/java/com/sust/sustcast/FeedbackFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 
-
 public class FeedbackFragment extends Fragment {
-
+    private Button bFeedback;
+    private Button bFacebook;
+    private Button bRate;
+    private Button bLogOut;
 
     public FeedbackFragment() {
         // Required empty public constructor
@@ -34,6 +37,51 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feedback, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_feedback, container, false);
+        bFeedback = rootView.findViewById(R.id.feedback_button);
+        bFacebook = rootView.findViewById(R.id.facebook_button);
+        bRate = rootView.findViewById(R.id.rate_button);
+        bLogOut = rootView.findViewById(R.id.logout_button);
+
+        bFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                giveFeedback();
+            }
+        });
+
+        bFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                visitFacebook();
+            }
+        });
+
+        bRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                giveRate();
+            }
+        });
+
+        bLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logOut();
+            }
+        });
+        return rootView;
+    }
+
+    private void logOut() {
+    }
+
+    private void giveRate() {
+    }
+
+    private void visitFacebook() {
+    }
+
+    private void giveFeedback() {
     }
 }
