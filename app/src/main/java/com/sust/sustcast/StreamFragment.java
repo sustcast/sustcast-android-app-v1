a0f119370017f961c35cbf37fb2b6ec36b95b32a
appledora <embers1010@gmail.com>
2020-06-01 19:43:28 +0600

    metadata mechanism finalized

--- app/src/main/java/com/sust/sustcast/StreamFragment.java
+++ app/src/main/java/com/sust/sustcast/StreamFragment.java
@@ -1,5 +1,6 @@
 package com.sust.sustcast;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -28,6 +32,11 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -36,12 +45,17 @@ import wseemann.media.FFmpegMediaMetadataRetriever;
 public class StreamFragment extends Fragment implements Player.EventListener {
 
     boolean isPlaying;
+    String iceURL;
+    FFmpegMediaMetadataRetriever fmmr;
+    DatabaseReference mDatabase;
+    Song song;
+    String name;
+    String artist;
     private SimpleExoPlayer exoPlayer;
     private Unbinder unbinder;
     private Button bPlay;
     private String TAG = "StreamFrag";
-    String iceURL;
-    FFmpegMediaMetadataRetriever fmmr;
+
     public StreamFragment() {
     }
 
@@ -65,6 +79,41 @@ public class StreamFragment extends Fragment implements Player.EventListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_stream, container, false);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                song = dataSnapshot.getValue(Song.class);
+                name = song.getName();
+                artist = song.getArtist();
+                Log.i(TAG, "Song name: " + name + ", artist " + artist);
+                Toast.makeText(getContext(), name + " - " + artist, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                song = dataSnapshot.getValue(Song.class);
+                name = song.getName();
+                artist = song.getArtist();
+                Log.i(TAG, "Song name: " + name + ", artist " + artist);
+                Toast.makeText(getContext(), name + " - " + artist, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         bPlay = rootView.findViewById(R.id.button_stream);
         unbinder = ButterKnife.bind(this, rootView);
         isPlaying = false;
@@ -87,7 +136,7 @@ public class StreamFragment extends Fragment implements Player.EventListener {
         });
 
         getPlayer();
-        getMetadata();
+        //getMetadata();
         return rootView;
     }
 
@@ -126,18 +175,24 @@ public class StreamFragment extends Fragment implements Player.EventListener {
 
     public void getMetadata() {
         fmmr = new FFmpegMediaMetadataRetriever();
-        fmmr.setDataSource(iceURL);
+        try {
+            fmmr.setDataSource(iceURL);
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "DjMeow is taking a nap. He will be back soon", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getContext(), LandingActivity.class));
+
+        }
         try {
             for (int i = 0; i < Constantss.METADATA_KEYS.length; i++) {
                 String key = Constantss.METADATA_KEYS[i];
                 String value = fmmr.extractMetadata(key);
-
-                if (value != null) {
-                    Log.i("METADATA => ", "Key: " + key + " Value: " + value);
-                } else {
-                    Log.i("METADATA => ", "Key: " + key + " Value: " + "NONE");
-
-                }
+//
+//                if (value != null) {
+//                    Log.i("METADATA => ", "Key: " + key + " Value: " + value);
+//                } else {
+//                    Log.i("METADATA => ", "Key: " + key + " Value: " + "NONE");
+//
+//                }
             }
         } catch (IllegalArgumentException ex) {
             ex.printStackTrace();
