ccc246456b8563cd093a8091051911dbe8083049
istiakshihab <istiak@pm.me>
2020-07-25 19:45:05 +0600

    Merges

--- app/src/main/java/com/sust/sustcast/utils/StringValidationRules.java
+++ app/src/main/java/com/sust/sustcast/utils/StringValidationRules.java
@@ -7,7 +7,7 @@ import android.util.Patterns;
 public class StringValidationRules {
         public static StringRule NOT_EMPTY = s -> TextUtils.isEmpty(s.toString());
         public static StringRule EMAIL = s -> !android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches();
-        public static StringRule PASSWORD = s -> s.length() < 8;
+        public static StringRule PASSWORD = s -> s.length() < 6;
         public static StringRule PHONE = s -> !Patterns.PHONE.matcher(s).matches();
 
         public interface StringRule {
