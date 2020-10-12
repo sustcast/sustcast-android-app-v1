934cb3ec74156b31981249e99bf64dfaedeaffb0
appledora <embers1010@gmail.com>
2020-10-12 14:30:34 +0600

    fragment context problem resolved

--- app/src/main/java/com/sust/sustcast/fragment/NewsReaderFragment.java
+++ app/src/main/java/com/sust/sustcast/fragment/NewsReaderFragment.java
@@ -30,7 +30,7 @@ public class NewsReaderFragment extends Fragment {
     private SimpleExoPlayer exoPlayer;
     private Unbinder unbinder;
     private Button bPlay;
-
+    View rootView;
     public NewsReaderFragment() {
     }
 
@@ -46,7 +46,7 @@ public class NewsReaderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_news_reader, container, false);
+        rootView = inflater.inflate(R.layout.fragment_news_reader, container, false);
         bPlay = rootView.findViewById(R.id.button_play);
         unbinder = ButterKnife.bind(this, rootView);
         NetworkInfoUtility networkInfoUtility = new NetworkInfoUtility();
@@ -58,14 +58,14 @@ public class NewsReaderFragment extends Fragment {
             @Override
             public void onPlayerError(ExoPlaybackException error) {
                 Crashlytics.logException(error);
-                Toast.makeText(getContext(), getString(R.string.server_off), Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), rootView.getContext().getString(R.string.server_off), Toast.LENGTH_LONG).show();
                 exoHelper.ToggleButton(false);
             }
         }, bPlay);
 
         isPlaying = true;
         setButton();
-        exoHelper.startExo(getString(R.string.bbc_news));
+        exoHelper.startExo(rootView.getContext().getString(R.string.bbc_news));
 
         return rootView;
     }
@@ -74,7 +74,7 @@ public class NewsReaderFragment extends Fragment {
         bPlay.setOnClickListener(view -> {
 
             if (!isPlaying) {
-                exoHelper.startExo(getString(R.string.bbc_news));
+                exoHelper.startExo(rootView.getContext().getString(R.string.bbc_news));
             } else {
                 exoHelper.stopExo();
             }
